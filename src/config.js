581601b6a1f8482c3adb909cdbe329044d054d8d
SINHA Santos <signme888@gmail.com>
2025-02-23 08:44:36 -0500

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,5 +1,17 @@
 // /srvra-sync/src/config.js
 
+/*
+ * Copyright (C) [2025] [Srvra]
+ * Author: [Santosh Sinha]
+ * This file is part of [SRVRA Enterprise UI Engine]
+ *
+ * This program is dual-licensed under either:
+ * - GNU Affero General Public License v3.0
+ * - Commercial License
+ * 
+ * Contact [signme888@gmail.com] for commercial licensing options
+ */
+
 export const DEFAULT_SYNC_CONFIG = {
     // Core sync settings
     syncInterval: 30000,
